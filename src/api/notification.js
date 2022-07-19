a63b05c3c59eccd11667c9a2ce014d3f1fde9700
achimande <41640645@qq.com>
2022-07-19 21:17:28 +0800

    unread notification

---
+++ src/api/notification.js
@@ -0,0 +1,4 @@
+import { authRequest } from '../utils/request'
+export function getNotificationStats(...params) {
+    return authRequest('notifications/stats', ...params)
+}
---
+++ src/store/modules/notification.js
@@ -0,0 +1,30 @@
+import { getNotificationStats } from '../../api/notification'
+const state = {
+    unreadCount: 0
+}
+var getters = {
+    unreadCount: (state) => state.unreadCount
+}
+const actions = {
+    async updateUnreadCount({ commit, getters }, params = {}) {
+        if (!getters.isLoggedIn) {
+            return
+        }
+
+        const statsResponse = await getNotificationStats({}, false)
+        commit('setUnreadCount', statsResponse.data.unread_count)
+    }
+}
+
+const mutations = {
+    setUnreadCount(state, count) {
+        state.unreadCount = count
+    }
+}
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
